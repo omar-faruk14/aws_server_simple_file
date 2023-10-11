ebdc5a2932ce9cafea7135c7ad1555acf3402fe2
Omar Faruk <Omarfaruk885@gmail.com>
2023-10-11 10:06:35 +0900

    Add Simple For aws nodejs Checking

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app=express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(3000, () =>{
+    console.log("Server is running on 3000 port");
+    
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello There");
+});
